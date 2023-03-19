6ad1ec59d22017ad184394f48192ca9798a64623
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-03-19 18:12:15 -0300

    Criando o diretório de templates no app contas, e adicionando o tamplete de home na view de home

--- contas/views.py
+++ contas/views.py
@@ -1,12 +1,10 @@
-'''from django.shortcuts import render'''
 import datetime
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def home (request):
-    now = datetime.datetime.now()
-    html = "<html><body> It is now %s. </body></html>" % now
-    return HttpResponse(html)
+    return render(request, 'contas/home.html')
 
