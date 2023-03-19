4953bac9abfb7b4dbe7f7679ef71ac02efd45230
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-03-19 18:57:14 -0300

    Criando o model Transacao

--- contas/models.py
+++ contas/models.py
@@ -8,3 +8,13 @@ class Categoria (models.Model):
     def __str__(self):
         return self.nome
 
+class Transacao(models.Model):
+    nome = models.CharField(max_length=25)
+    data_transacao = models.DateTimeField(auto_now_add=False)
+    descricao = models.TextField()
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    observacoes = models.TextField()
+    Categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.nome
