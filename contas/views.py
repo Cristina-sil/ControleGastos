1f7e92b70f53ea9b5391fadfc4ed64260bb5ba8e
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-03-22 21:31:46 -0300

    CRUD Transacao - Create

--- contas/views.py
+++ contas/views.py
@@ -1,15 +1,27 @@
 import datetime
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from .forms import TransacaoForm
 from .models import Transacao
 
-# Create your views here.
 
 def home (request):
     transacao = Transacao.objects.all()
     return render(request, 'contas/home.html',context={
         'transacao': transacao,    
     })
+
+def cadastrarTransacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('url_home')
+    data['form'] = form
+    return render(request, 'contas/form.html',data)
+
+
+
 
